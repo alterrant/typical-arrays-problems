640887e9d837ab9a2f4f64c749d0291f11135f52
”alterrant” <pandor246@mail.ru>
2021-03-09 18:49:21 +0300

    feat: add solustion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!arguments.length == true || array.length === 0) return 0;
+    else return array.reduce((a, b) => (a > b) ? b : a, 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!arguments.length == true || array.length === 0) return 0;
+    else return array.reduce((a, b) => (a > b) ? a : b, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!arguments.length == true || array.length === 0) return 0;
+    else return (array.reduce((a, b) => (a + b), 0)/array.length);
 }
